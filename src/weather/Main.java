e746414181d0f0bf6557ca5b2a22efc868265214
Małgorzata Łepek <gosialepek@gmail.com>
2017-11-22 23:48:45 +0100

    Initial commit

---
+++ src/weather/Main.java
@@ -0,0 +1,13 @@
+package weather;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Weather today = new Weather(LocalDate.now(), WeatherInfo.CLOUDY);
+        Weather yesterday = new Weather(LocalDate.of(2015, 5, 8), WeatherInfo.RAINY);
+
+        System.out.println(today.getWeatherInfo().getDescription());
+        System.out.println(yesterday.getWeatherInfo().getAvgTemp());
+    }
+}
---
+++ src/weekDays/Main.java
@@ -0,0 +1,9 @@
+package weekDays;
+
+public class Main {
+    public static void main(String[] args) {
+        DayOfWeek day = DayOfWeek.CZWARTEK;
+        System.out.println(day.getNumberOfDay());
+        // NIE DA SIE: DayOfWeek dayOfWeek =  new DayOfWeek();
+    }
+}
